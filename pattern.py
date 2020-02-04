496cd995dc4517e75928ea694d0479f86943572e
Balasaranya V <41548678+balasaranyav@users.noreply.github.com>
2020-02-04 12:17:47 +0530

    pattern
    diamond swapping pattern

--- pattern.py
+++ pattern.py
@@ -1,5 +1,18 @@
-n=int(input("Enter no.of rows:"))
-for i in range(0,n+1):  
-    for j in range(0, i): 
-        print("* ",end="") 
-    print("\r") 
+n=6
+k=7
+for i in range(n,0,-1):
+    for j in range(0,k+1):
+        print(end=" ")
+    k=k+1
+    for i in range(0,i):
+        print("* ",end="")
+    print("\r")
+for i in range(0,n):
+    for j in range(0,k):
+        print(end=" ")
+    k=k-1
+    for i in range(0,i+1):
+        print("* ",end="")
+    print("\r")
+    
+
